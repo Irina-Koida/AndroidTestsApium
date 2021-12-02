5aa02528c38a235f8e448e2bf7db748ff9c9c37a
Irina-Koida <91370760+Irina-Koida@users.noreply.github.com>
2021-12-02 18:04:35 +0200

    Add project files.

---
+++ AndroidTestsApium/Drivers/AppiumDriver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Appium.Service;
+
+namespace AndroidTestsApium.Drivers
+{
+    class AppiumDriver
+    {
+        private AppiumDriver<AndroidElement> _driver;
+        public AppiumDriver<AndroidElement> InitializeAppium()
+        {
+            var driverOptions = new AppiumOptions();
+            driverOptions.AddAdditionalCapability(MobileCapabilityType.PlatformName, "Android");
+            driverOptions.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, "10.0");
+            driverOptions.AddAdditionalCapability(MobileCapabilityType.DeviceName, "Samsung Galaxy S10");
+            driverOptions.AddAdditionalCapability(MobileCapabilityType.App, "C:\\Users\\Irina Koida\\Downloads\\vivino.web.app_8.20.10_8201099.apk");
+            driverOptions.AddAdditionalCapability(MobileCapabilityType.AutomationName, "Appium");
+
+            var appiumService = new AppiumServiceBuilder().WithIPAddress("127.0.0.1")
+                .UsingAnyFreePort().Build(); //запускаем сервер
+
+            if (!appiumService.IsRunning)
+            {
+                appiumService.Start();
+            }
+
+            _driver = new AndroidDriver<AndroidElement>(appiumService, driverOptions, TimeSpan.FromSeconds(100));
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            return _driver;
+        }
+    }
+}
---
+++ AndroidTestsApium/Hooks/Hooks.cs
@@ -0,0 +1,30 @@
+﻿using AndroidTestsApium.Drivers;
+using OpenQA.Selenium.Appium.Android;
+using TechTalk.SpecFlow;
+
+namespace AndroidTestsApium.Hooks
+{
+    [Binding]
+    public sealed class Hooks
+    {
+        private readonly ScenarioContext _scenarioContext;
+
+        public Hooks (ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            AppiumDriver appiumDriver = new AppiumDriver();
+            _scenarioContext.Set(appiumDriver.InitializeAppium(), "driver");
+        }
+
+        //[AfterScenario]
+        //public void AfterScenario()
+        //{
+        //    _scenarioContext.Get<AndroidDriver<AndroidElement>>("driver").Dispose();
+        //}
+    }
+}
