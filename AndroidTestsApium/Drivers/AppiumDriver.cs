69e81853b7fc54ce9d59352fed9ec819feff2917
DESKTOP-M25GUR8\Kostiantin <k.galchenko27@gmail.com>
2021-12-04 23:41:36 +0200

    commit done

--- AndroidTestsApium/Drivers/AppiumDriver.cs
+++ AndroidTestsApium/Drivers/AppiumDriver.cs
@@ -13,11 +13,13 @@ namespace AndroidTestsApium.Drivers
         public AppiumDriver<AndroidElement> InitializeAppium()
         {
             var driverOptions = new AppiumOptions();
+
             driverOptions.AddAdditionalCapability(MobileCapabilityType.PlatformName, "Android");
+            driverOptions.AddAdditionalCapability(MobileCapabilityType.DeviceName, "Android SDK");
             driverOptions.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, "10.0");
-            driverOptions.AddAdditionalCapability(MobileCapabilityType.DeviceName, "Samsung Galaxy S10");
-            driverOptions.AddAdditionalCapability(MobileCapabilityType.App, "C:\\Users\\Irina Koida\\Downloads\\vivino.web.app_8.20.10_8201099.apk");
             driverOptions.AddAdditionalCapability(MobileCapabilityType.AutomationName, "Appium");
+            driverOptions.AddAdditionalCapability(MobileCapabilityType.App, "C:\\Users\\Kostiantin\\Desktop\\TestsAPK\\vivino.web.app_8.20.10_8201099.apk");
+            driverOptions.AddAdditionalCapability("appWaitActivity", "com.vivino.activities.OOTB");
 
             var appiumService = new AppiumServiceBuilder().WithIPAddress("127.0.0.1")
                 .UsingAnyFreePort().Build(); //запускаем сервер
